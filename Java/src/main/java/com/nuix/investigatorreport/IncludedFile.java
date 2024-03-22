02c7a6c311e3ca9134f31558ecb655afe446a3f5
Jason Wells <jason.wells@nuix.com>
2024-03-22 10:23:22 -0700

    Initial IntelliJ project commit

--- Java/src/main/java/com/nuix/investigatorreport/IncludedFile.java
+++ Java/src/main/java/com/nuix/investigatorreport/IncludedFile.java
@@ -3,18 +3,22 @@ package com.nuix.investigatorreport;
 import java.io.File;
 
 public class IncludedFile {
-	private String title;
-	private File filePath;
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public File getFilePath() {
-		return filePath;
-	}
-	public void setFilePath(File filePath) {
-		this.filePath = filePath;
-	}
+    private String title;
+    private File filePath;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public File getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(File filePath) {
+        this.filePath = filePath;
+    }
 }
